136d29a273512832502d04a9429329771a45d5cd
Carlos Moraes <78217618+Carlosvpm@users.noreply.github.com>
2021-03-11 11:11:11 -0300

    adicionando projeto
    Pedidos de restaurante

---
+++ algoritimo_de_pedidos.py
@@ -0,0 +1,47 @@
+# Lista para cardápio:
+produtos = [[100, "cachorro-quente", 4.50], [101, "Hamburger", 5.00], [102, "Misto", 2.75], [103, "Refrigerante", 3.50]]
+
+#lista produtos comprados
+preço_produtos_comprados = []
+nome_produtos_comprados = []
+
+# Função de verificar se o código digitado corresponde a um produto:
+def buscar_produtos():
+    encontrar = 0
+    for x in produtos:
+        if codigo_digitado == x[0]:
+            print(x[1])
+            encontrar = 1
+            if encontrar != 1:
+                print("Código inválido")
+
+# Função adicionar preço na lista de preços dos produtos comprados: 
+def adicionar_preço():
+    for x in produtos:
+        if codigo_digitado == x[0]:
+            preço = x[2]
+            preço_produtos_comprados.append(x[2])
+# Função adicionar nome dos produtos comprados na respectiva lista:
+def adicionar_nomes():
+    for x in produtos:
+        if codigo_digitado == x[0]:
+            nome = x[1]
+            nome_produtos_comprados.append(x[1])
+
+#primeiro pedido:
+codigo_digitado = int(input("Código do produto: "))
+buscar_produtos()
+adicionar_preço()
+adicionar_nomes()
+resposta = input("Deseja continuar?")
+
+#Quando quiser adicionar mais pedidos
+while resposta == "sim":
+    codigo_digitado = int(input("Código do produto: "))
+    buscar_produtos()
+    adicionar_preço()
+    adicionar_nomes()
+    resposta = input("Deseja continuar?")
+else:
+    print(f"Valor total a pagar: {sum(preço_produtos_comprados)}")
+    print(nome_produtos_comprados)
